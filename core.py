6a68e5d4248a8411b488590c3e78dfac3aa487e0
Gustavo Freitas <gustavo-silvafreitas@hotmail.com>
2021-03-06 12:53:59 -0300

    Enviar core.py atualizado

--- core.py
+++ core.py
@@ -1,24 +1,49 @@
 import os
+import functions
 
 tabuleiro =[
-    ['_','_','_'],
-    ['_','_','_'],
-    ['_','_','_']
+    [' ','A','B','C'],
+    ['1','_','_','_'],
+    ['2','_','_','_'],
+    ['3','_','_','_']
 ]
 
 print("Bem vindo ao Jogo da velha!")
-ops = input("Informe qual modo você queira jogar:\n1- Contra Player\n2- Contra bot \n")
+print("Você no momento está jogando contra um bot! \n \n")
+
+#opções para jogar
+""" ops = int(input("Informe qual modo você queira jogar:\n1- Contra Player\n2- Contra bot \n"))
+
+while (ops != 1 and ops != 2):
+    ops = int(input("Opção incorreta! Por gentileza, informe novamente o modo que você queira jogar:\n1- Contra Player\n2- Contra bot \n"))
+
+if(ops == 1):
+    print("Teste")
+
+if(ops == 2):
+    print("Teste1") """
 
 #Imprimir tabuleiro
-""" i = 0
-while(i<3):
+i = 0
+while(i<4):
     i2 = 0
-    while(i2 <3):
-        if i2 == 2:
+    while(i2 <4):
+        if i2 == 3:
             print(tabuleiro[i][i2])
         else:
             print(tabuleiro[i][i2], end=' ')
         i2 += 1
     i += 1
- """
+
+print("\n \n")
+
+jogadaPlayer = input("Escolha as coordenadas onde deseja jogar:")
+jogadaPlayer = jogadaPlayer.replace(" ","") #Retirar possiveis espaços da coordenada
+jogadaPlayer = jogadaPlayer.lower() #Parametrizar as coordenadas
+
+while(functions.verificadorJogada(jogadaPlayer) == False):
+    jogadaPlayer = input("Coordenadas incorretas! Por favor, preencha corretamente as coordenadas onde deseja jogar:")
+    jogadaPlayer = jogadaPlayer.replace(" ","") #Retirar possiveis espaços da coordenada
+    jogadaPlayer = jogadaPlayer.lower() #Parametrizar as coordenadas
+
 os.system("pause")
